7a4e086104bebb58418255c0a48ab03b8484c8c7
CAEmario <CAEAddress@gmail.com>
2017-11-08 14:57:07 +0100

    MicroserviceCall edited by Luigi Test

--- js/applicationScript.js
+++ js/applicationScript.js
@@ -62,15 +62,10 @@ var onContinueClicked = function(){
   client.sendRequest("GET", "getQuiz/"+window.currentQuestionId, "", "", {}, false,
   function(data, type) {
     if(Object.keys(data).length > 0){
-
 	    $('# id of 1st radio button').parent().contents().last()[0].textContent=data.answerA;
-
 	    $('# id of 2th radio button').parent().contents().last()[0].textContent=data.answerB;
-
 	    $('# id of 3th radio button').parent().contents().last()[0].textContent=data.answerC;
-
 	    $('# id of 4th radio button').parent().contents().last()[0].textContent=data.answerD;
-
 	    $('# id of the video').attr('src',data.videolink);
 	
     	    $("# id of the question").text(data.question);
